c7c967304b06b591aca26468cbc06f5994d30639
RootLeo00 <caterina.leonelli@gmail.com>
2023-11-01 22:02:21 +0100

    open zeppelin works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,104 +1,141 @@
 import { useEffect, useMemo, useRef, useState } from 'react'
-//import styles from './styles.module.css'
+import styles from './styles.module.css'
 import * as ethereum from '@/lib/ethereum'
 import * as main from '@/lib/main'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useWallet } from "./utilities"
-import { LoginPage } from "./pages/LoginPage";
-import { AdminPage } from "./pages/AdminPage";
-import { UserPage } from "./pages/UserPage";
-import {BigNumber, utils} from 'ethers'
 
+type Canceler = () => void
+const useAffect = (
+  asyncEffect: () => Promise<Canceler | void>,
+  dependencies: any[] = []
+) => {
+  const cancelerRef = useRef<Canceler | void>()
+  useEffect(() => {
+    asyncEffect()
+      .then(canceler => (cancelerRef.current = canceler))
+      .catch(error => console.warn('Uncatched error', error))
+    return () => {
+      if (cancelerRef.current) {
+        cancelerRef.current()
+        cancelerRef.current = undefined
+      }
+    }
+  }, dependencies)
+}
 
+const useWallet = () => {
+  const [details, setDetails] = useState<ethereum.Details>()
+  const [contract, setContract] = useState<main.Main>()
+  useAffect(async () => {
+    const details_ = await ethereum.connect('metamask')
+    if (!details_) return
+    setDetails(details_)
+    const contract_ = await main.init(details_)
+    if (!contract_) return
+    setContract(contract_)
+  }, [])
+  return useMemo(() => {
+    if (!details || !contract) return
+    return { details, contract }
+  }, [details, contract])
+}
 
-
-/*
 export const App = () => {
+
   const wallet = useWallet()
-
-
-  return (
-
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<AdminPage />}>
-
-      </Route>
-    </Routes>
-    </BrowserRouter>
-
-  )
-}*/
-export const App = () => {
-  const wallet = useWallet()
-  const adminAccount = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+  const adminAccount = wallet?.details.account
+  console.log('Admin', adminAccount)
 
   useEffect(() => {
-    console.log(wallet?.details)
-    // return Promise.resolve()
+    console.log('Wallet', wallet)
   }, [wallet])
-  
 
-  const isEmptyAccount = !wallet?.details.account;
-  const isAdmin = wallet?.details.account ===adminAccount ;
-  console.log("app")
-  //mie modifiche per poter individuare il login in metamask e se non è stato effettuato lo indirizzo alla pagine di login altrimenti se è un admin vado in admin page e se è uno user vado in user page
-  //poi nella login page inserisco uno useAffect che controlli che ci siano state modifiche nell'account e se non ci sono state continua a mostrare please login, mentre se ci sono state rimanda alle due pagine admin e user a seconda dell'utente loggato
-  //inserire una default page per indirizzi non validi e poi un aggiornamento della pagina nel caso in cui un utente dovesse cambiare account e per esempio passare da admin a user
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path= "/" element={<LoginPage />}/>
-        <Route path="/UserPage" element={<UserPage />} />
-        <Route path="/AdminPage" element={<AdminPage />} />
-        <Route path="/LoginPage" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
 
-  /*<Route
-          path="/"
-          element={
-            isEmptyAccount ? (
-              <LoginPage />
-            ) : isAdmin ? (
-              <AdminPage />
-            ) : (
-              <UserPage />
-            )
-          }
-        */
-  /*
-  
-  const [balance, setBalance] = useState<string>("0")
-  const [info, setInfo] = useState<string>("connecting")
-  const [contractAddress, setContractAddress] = useState<string>("")
-
-  async function depositEth() {
-    wallet?.contract.Deposit({ value: utils.parseEther("1") }).then((trx) => {
-      console.log(trx)
-    })
+  const addCollection = () => {
+    console.log('Add collection', wallet)
+    wallet?.contract
+      .createCollection('collection1', 100)
+      .then((res: any) => {
+        console.log(res)
+      })
+      .catch((e: any) => {
+          console.log(e)
+        })
+        .finally(() => {
+          console.log("added collection")
+        })
   }
 
-  useAffect(() => {
-    setInfo(String(wallet?.details.account))
-    wallet?.contract.seeBalance().then((num: BigNumber) => {
-      setBalance(utils.formatEther(num))
-    })
-    setContractAddress(String(wallet?.contract.address))
+  const getAllCollections = () => {
+    console.log('Get all collections')
+    wallet?.contract.getAllCollections().then(console.log).catch(console.error)
+  }
 
-    return Promise.resolve()
-  }, [wallet])
+  const mintAndAssignCards = () => {
+    console.log('Mint and assign cards')
+    wallet?.contract
+      .mintAndAssignCards('collection1', ['card1','card2','card3'])
+      .then(console.log)
+      .catch(console.error)
+  }
+
+  const assignCardToAUser = () => {
+    console.log('Assign card to a user')
+    wallet?.contract
+      .assignCardToUser('card2', adminAccount)
+      .then(console.log)
+      .catch(console.error)
+  }
+
+const getCardsOfACollection = () => {
+  console.log('Get cards of a collection')
+  wallet?.contract
+    .getCardsFromCollection('collection1')
+    .then(console.log)
+    .catch(console.error)
+}
+
+const getCardsOfAUser = () => {
+  console.log('Get cards of a user')
+  wallet?.contract
+    .getCardsOfUser(adminAccount)
+    .then(console.log)
+    .catch(console.error)
+}
+
+const userOwnsCard = () => {
+  console.log('User owns card')
+  wallet?.contract
+    .userOwnsCard(adminAccount, 'card2')
+    .then(console.log)
+    .catch(console.error)
+}
 
   return (
-    <div>
-      
-      <button onClick={depositEth}>Deposit 1 ETH</button>
-      <p>Account {info}</p>
-      <p>Contract {contractAddress}</p>
-      <p>Balance on the contract {balance} ETH</p>
+    <div className={styles.body}>
+      <h1>Welcome to Pokémon TCG</h1>
+      <div>
+        <button type="button" onClick={addCollection}>
+          Add collection{' '}
+        </button>
+        <button type="button" onClick={getAllCollections}>
+          collections{' '}
+        </button>
+        <button type="button" onClick={mintAndAssignCards}>
+          Mint and assign cards{' '}
+          </button>
+          <button type="button" onClick={getCardsOfACollection}>
+          Retrieve cards from a collection name{' '}
+          </button>
+          <button type="button" onClick={assignCardToAUser}>
+          Assign a card to a user{' '}
+          </button>
+          <button type="button" onClick={getCardsOfAUser}>
+          Retrieve cards Of a user address{' '}
+          </button>
+          <button type="button" onClick={userOwnsCard}>
+          User owns a card{' '}
+          </button>
+      </div>
     </div>
-  )*/
-
-
+  )
 }
