ec8449a94f28a3b3e36700159b323d78c2de3d82
maria cristina centorame <115945348+mcri00@users.noreply.github.com>
2023-11-03 21:26:06 +0100

    search bar

--- frontend/src/components/CollectionsTreeView.jsx
+++ frontend/src/components/CollectionsTreeView.jsx
@@ -8,13 +8,85 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 
 const CollectionsTreeView = ({ collectionsData, navigate }) => {
-    const [collectionSearchText, setCollectionSearchText] = useState('');
-
-    return(
+    //const [collectionSearchText, setCollectionSearchText] = useState('');
+    const [searchText, setSearchText] = useState('');
+    const filteredCollections = collectionsData
+                  .filter((collection) => {
+                    return (
+                      collection.name.toLowerCase().includes(searchText.toLowerCase()) ||
+                      collection.cards.some((card) =>
+                        card.url.toLowerCase().includes(searchText.toLowerCase())
+                      )
+                    );
+                  });
+    return (
+      <div>
+          <h2>Collections</h2>
+          <div>
+              {/* Searchbar for collections and cards */}
+              <TextField
+                  label="Search collections and cards"
+                  variant="outlined"
+                  value={searchText}
+                  onChange={(e) => setSearchText(e.target.value)}
+              />
+          </div>
+          <TreeView
+              defaultCollapseIcon={<ExpandMoreIcon />}
+              defaultExpandIcon={<ChevronRightIcon />}
+          >
+              {filteredCollections.map((collection, collectionIndex) => (
+                  
+                  <TreeItem
+                      key={collectionIndex}
+                      nodeId={`collection-${collectionIndex}`}
+                      label={
+                          <Link
+                              component="button"
+                              onClick={() => navigate(`/CollectionInfoPage/${collection.name}`)}
+                              style={{ cursor: 'pointer' }}
+                          >
+                              {collection.name}
+                          </Link>
+                      }
+                  >
+                      {collection.cards.map((card, cardIndex) => {
+                          if (
+                              collection.name.toLowerCase().includes(searchText.toLowerCase()) ||
+                              card.url.toLowerCase().includes(searchText.toLowerCase())
+                          ) {
+                              return (
+                                  <TreeItem
+                                      key={`card-${collectionIndex}-${cardIndex}`}
+                                      nodeId={`card-${collectionIndex}-${cardIndex}`}
+                                      label={
+                                          <Link
+                                              component="button"
+                                              onClick={() => navigate(`/CardInfoPage/${card.id}`)}
+                                              style={{ cursor: 'pointer' }}
+                                          >
+                                              {card.url}
+                                          </Link>
+                                      }
+                                  />
+                              );
+                          } else {
+                              return null;
+                          }
+                      })}
+                  </TreeItem>
+))}
+          </TreeView>
+      </div>
+  );
+}
+export default CollectionsTreeView;
+/*create a page called MintingPage that will have a stepper component(from the mui librar to go first to the creation of the collection(choice of the name and of the maximum number of cards allowed), selection of cards, selection of the user*/
+/*    return(
         <div>
         <h2>Collections</h2>
         <div>
-            {/* Searchbar for collections */}
+            Searchbar for collections 
             <TextField
               label="Search collections"
               variant="outlined"
@@ -43,7 +115,10 @@ const CollectionsTreeView = ({ collectionsData, navigate }) => {
                 }
               >
               {
-                collection.cards.map((card, cardIndex) => (
+                collection.cards
+                .filter((card) =>
+                  card.url.toLowerCase().includes(collectionSearchText.toLowerCase())
+                ).map((card, cardIndex) => (
                   <TreeItem
                     key={`card-${collectionIndex}-${cardIndex}`}
                     nodeId={`card-${collectionIndex}-${cardIndex}`}
@@ -65,5 +140,4 @@ const CollectionsTreeView = ({ collectionsData, navigate }) => {
         </TreeView>
         </div>
     );
-}
-export default CollectionsTreeView;
+    */
