6a30cf6f6dec403e37b626cb02074f809854c9e1
ubombar <ufukbombar@gmail.com>
2023-11-05 23:44:53 +0100

    Working stable verwion xxl super deluxe extreeme

--- frontend/src/pages/auctions/AuctionInfoPage.tsx
+++ frontend/src/pages/auctions/AuctionInfoPage.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { checkAccount, useWallet } from '@/utilities'
 import styles from '../../styles.module.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid, Button } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { CardContent, CardActions, Typography } from '@mui/material';
@@ -25,53 +25,73 @@ const mockDataAuction = {
 
 
 export const AuctionInfoPage = () => {
+    const wallet = useWallet();
     const { ID } = useParams();
     let auctionId = parseInt(ID || "");
     const navigate = useNavigate();
     const [noBidder, setNoBidder] = useState(false);
     const [showOffer, setShowOffer] = useState(false);
-    const wallet = useWallet();
+    const [disabled, setDisabled] = useState(true);
+
     const [auctionData, setAuctionData] = useState({
         sellerAddress: "0x0000000000000000000000000000000000000000"
     });
+    const [currentWalletAddress, setCurrentWalletAddress] = useState(wallet?.details.account?.toLowerCase());
 
     // Get the auction id info
     function acceptAndExchange() {
-
+        wallet?.marketContract.acceptAndExchange(auctionId).then(() => {
+            console.log("auction completed!");
+        })
     }
 
     function rejectOffer() {
-
+        wallet?.marketContract.reject(auctionId).then(() => {
+            console.log("auction reject!");
+        })
     }
 
     function cancelAuction() {
-
+        wallet?.marketContract.cancel(auctionId).then(() => {
+            console.log("auction cancel!");
+        })
     }
 
-    wallet?.marketContract.list().then(async (ret: ethers.BigNumber[]) => {
-        let statusToString = {
-            0: "Not Open",
-            1: "Open",
-            2: "Concluded",
-            3: "Cancelled"
+    useEffect(() => {
+        const newFunc = async () => {
+            let statusToString = {
+                0: "Not Open",
+                1: "Open",
+                2: "Concluded",
+                3: "Cancelled"
+            }
+
+            let statusInt = await wallet?.marketContract.statusOf(auctionId);
+            let biddersAddress: string = await wallet?.marketContract.currentBidderOf(auctionId);
+
+            let auctionItem = {
+                auctionId: auctionId,
+                statusInt: statusInt,
+                status: statusToString[statusInt], // ignore the warning
+                sellerAddress: await wallet?.marketContract.sellerOf(auctionId),
+                biddderAddress: await wallet?.marketContract.currentBidderOf(auctionId),
+                sellersToken: (await wallet?.marketContract.sellersTokenIdOf(auctionId)).toString(),
+                biddersToken: (await wallet?.marketContract.currentBiddersTokenIdOf(auctionId)).toString(),
+                hasBidder: !biddersAddress.includes("0x0000000000000000000000000000000000000000"),
+            }
+
+            setAuctionData(auctionItem);
+            setDisabled(auctionItem.sellerAddress.toLowerCase() != wallet?.details.account.toLowerCase())
+
+
+            // console.log(auctionItem.sellerAddress.toLowerCase());
+            // console.log(wallet?.details.account.toLowerCase());
+            // console.log(auctionItem.sellerAddress.toLowerCase());
+            // console.log(auctionItem.sellerAddress.toLowerCase() == wallet?.details.account.toLowerCase());
+
         }
-
-        let statusInt = await wallet.marketContract.statusOf();
-        let biddersAddress: string = await wallet.marketContract.currentBidderOf(auctionId);
-
-        let auctionItem = {
-            auctionId: auctionId,
-            statusInt: statusInt,
-            status: statusToString[statusInt], // ignore the warning
-            sellerAddress: await wallet.marketContract.sellerOf(auctionId),
-            biddderAddress: await wallet.marketContract.currentBidderOf(auctionId),
-            sellersToken: (await wallet.marketContract.sellersTokenIdOf(auctionId)).toString(),
-            biddersToken: (await wallet.marketContract.currentBiddersTokenIdOf(auctionId)).toString(),
-            hasBidder: !biddersAddress.includes("0x0000000000000000000000000000000000000000"),
-        }
-
-        setAuctionData(auctionItem);
-    })
+        newFunc()
+    }, [wallet])
 
     return (
         <Grid container spacing={3}>
@@ -81,13 +101,13 @@ export const AuctionInfoPage = () => {
                 <AuctionCardDetailed data={auctionData} navigate={navigate} noBidder={noBidder} showOffer={showOffer}></AuctionCardDetailed>
             </Grid>
             <Grid item xs={4}>
-                <Button onClick={acceptAndExchange} disabled={wallet?.details.account?.toLowerCase() == auctionData.sellerAddress}>Accept and Exchange</Button>
+                <Button onClick={acceptAndExchange} disabled={disabled}>Accept and Exchange</Button>
             </Grid>
             <Grid item xs={4}>
-                <Button onClick={rejectOffer} disabled={wallet?.details.account?.toLowerCase() == auctionData.sellerAddress}>Reject</Button>
+                <Button onClick={rejectOffer} disabled={disabled}>Reject</Button>
             </Grid>
             <Grid item xs={4}>
-                <Button onClick={cancelAuction} disabled={wallet?.details.account?.toLowerCase() == auctionData.sellerAddress}>Cancel Auction</Button>
+                <Button onClick={cancelAuction} disabled={disabled}>Cancel Auction</Button>
             </Grid>
         </Grid>
     );
