0881a9f60dde9c65f3597aa14f976f39ab06cb28
RootLeo00 <caterina.leonelli@gmail.com>
2023-11-05 21:39:07 +0100

    display user cards

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -1,13 +1,16 @@
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { checkAccount } from '@/utilities'
 import styles from '../styles.module.css'
-import { Grid, Button } from '@mui/material';
+import { Grid, Button, Stack } from '@mui/material';
+import { useWallet } from "../utilities"
 
 import UsersList from "../components/UsersList";
 import CollectionsTreeView from "../components/CollectionsTreeView";
 
 import ShowPath from '../components/ShowPath';
+import CardMTG from '@/components/CardMTG';
+import { ethers } from 'ethers';
 //layout
 const usersData = [
   { id: 1, name: 'User 1', otherInfo: '...' },
@@ -50,15 +53,32 @@ const collectionsData = [
 
 export const UserPage = () => {
   const [number, setNumber] = useState<number>(0)
-  console.log("ciao")
+  const [cards, setCards] = useState<string[]>([]); 
+  const wallet = useWallet();
+
+  useEffect(() => {
+    if (wallet?.details.account) {
+        wallet?.cardmanagerContract.userToCards(wallet?.details.account)
+    }
+}, [wallet]);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      const response = wallet?.cardmanagerContract.userToCards(wallet.details.account)
+      .then((cardArray: string[]) => {
+        console.log(cardArray)
+        // remove strings that has length less than 36 char
+        cardArray = cardArray.filter((cardId) => cardId.length >= 36) //TODO: regex better formatting
+          setCards(cardArray);
+          console.log(cardArray)
+      })
+    }
+    fetchCards();
+  }
+  , [wallet?.cardmanagerContract]);
 
   const navigate = useNavigate();
   checkAccount(navigate)
-
-  function buttonClick() {
-    console.log("1 click")
-    setNumber(number + 1)
-  }
 
   function handleAuctionClick() {
     navigate("/UserPage/AuctionPage")
@@ -70,7 +90,12 @@ export const UserPage = () => {
       <h2>Inventory: my cards</h2>
       <Grid container spacing={3}>
         <Grid item xs={6} style={{ alignItems: 'center' }}>
-          <CollectionsTreeView collectionsData={collectionsData} navigate={navigate} />
+          {/* <CollectionsTreeView collectionsData={collectionsData} navigate={navigate} /> */}
+          <Stack spacing={{ xs: 1, sm: 2 }} direction="row" useFlexGap flexWrap="wrap">
+                {cards?.map((id, index) => (
+                    <CardMTG key={index} id={`${id}`} />
+                ))}
+            </Stack>
         </Grid>
         <Grid item xs={6} style={{ alignItems: 'center' }}>
           <UsersList usersData={usersData} navigate={navigate} />
